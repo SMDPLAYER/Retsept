b3163731c6e28d27dfdbfc7a99643bcfe551fd44
mukhriddin <smdplayer1999@gmail.com>
2023-01-28 14:52:50 +0500

    Initial commit

---
+++ app/src/main/java/uz/smd/retsept/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.smd.retsept
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
